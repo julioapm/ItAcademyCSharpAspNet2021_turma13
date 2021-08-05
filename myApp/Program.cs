7351ce6638f98b1de38bc87420b67075ba0baf7c
Júlio Pereira Machado <julioapm@gmail.com>
2021-08-05 15:53:20 -0300

    Alteração para imprimir data

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -7,6 +7,7 @@ namespace myApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
         }
     }
 }
