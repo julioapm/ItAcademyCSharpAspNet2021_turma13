05572f00de4dc69cae830b1c20a27558adaf718a
Júlio Pereira Machado <julioapm@gmail.com>
2021-08-11 15:56:40 -0300

    Exemplo básico de delegate

---
+++ DemoDelegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DemoDelegate
+{
+    public delegate void ReferenciaMetodoVoid();
+    public delegate void ReferenciaMetodoVoidComParametro(int x);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReferenciaMetodoVoid refm = AloMundo;
+            refm();
+            ReferenciaMetodoVoidComParametro refm2 = AloNumero;
+            refm2(10);
+        }
+
+        static void AloMundo()
+        {
+            Console.WriteLine("Alo mundo");
+        }
+
+        static void AloNumero(int x)
+        {
+            Console.WriteLine("Alo {0}", x);
+        }
+    }
+}
