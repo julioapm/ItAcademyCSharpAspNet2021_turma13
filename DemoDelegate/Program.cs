a432aea653bc30b5200446e4222f1ffac4e16279
Júlio Pereira Machado <julioapm@gmail.com>
2021-08-11 16:44:47 -0300

    Código final

--- DemoDelegate/Program.cs
+++ DemoDelegate/Program.cs
@@ -2,6 +2,7 @@
 
 namespace DemoDelegate
 {
+    public delegate R MeuDelagate<T,R>(T x);
     public delegate void ReferenciaMetodoVoid();
     public delegate void ReferenciaMetodoVoidComParametro(int x);
     class Program
@@ -12,6 +13,16 @@ namespace DemoDelegate
             refm();
             ReferenciaMetodoVoidComParametro refm2 = AloNumero;
             refm2(10);
+            ReferenciaMetodoVoid refm3 = () => Console.WriteLine("Alo Mundo");
+            ReferenciaMetodoVoidComParametro refm4 = x => Console.WriteLine(x);
+            int resultado = FazAlgo((x,y) => x*y, 5);
+            Console.WriteLine(resultado);
+
+            var acoisa = ConstroiMetodo(5);
+            int resultado2 = acoisa(10);
+            Console.WriteLine(resultado2);
+
+            MeuDelagate<string,int> refm5 = x => x.Length;
         }
 
         static void AloMundo()
@@ -23,5 +34,17 @@ namespace DemoDelegate
         {
             Console.WriteLine("Alo {0}", x);
         }
+
+        static int FazAlgo(Func<int,int,int> f, int x)
+        {
+            return f(x, x);
+        }
+
+        static Func<int,int> ConstroiMetodo(int y)
+        {
+            return (x) => x + y;
+        }
+
+
     }
 }
