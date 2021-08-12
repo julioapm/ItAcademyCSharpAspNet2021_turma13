9c64097cefa9e94f86b268dc3282fa481a1603bf
Júlio Pereira Machado <julioapm@gmail.com>
2021-08-12 14:34:50 -0300

    Código final

---
+++ DemoAsync/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DemoAsync
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Vou fazer um cálculo demorado");
+            var resultado = await FazCalculoDemoradoAsync(10);
+            Console.WriteLine("Resultado = " + resultado);
+        }
+
+        static async Task<int> FazCalculoDemoradoAsync(int x)
+        {
+            Console.WriteLine("Calculando...");
+            await Task.Delay(5000);
+            Console.WriteLine("Finalizando o cálculo");
+            return (int)Math.Pow(2,x);
+        }
+    }
+}
