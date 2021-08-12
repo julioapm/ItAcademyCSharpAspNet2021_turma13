39c41359e2624b7755fe350924d9e21514723809
Júlio Pereira Machado <julioapm@gmail.com>
2021-08-12 15:35:28 -0300

    Código final

---
+++ DemoAsync2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace DemoAsync2
+{
+    class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            /*
+            string uri = "https://viacep.com.br/ws/01001000/json/";
+            var resposta = await client.GetAsync(uri);
+            Console.WriteLine(resposta.StatusCode);
+            Console.WriteLine(resposta.IsSuccessStatusCode);
+            var tarefa = resposta.Content.ReadAsStringAsync();
+            //var json = tarefa.Result;
+            var json = await tarefa;
+            Console.WriteLine(json);
+            */
+            /*
+            try
+            {
+                string uri = "https://viacep.com.br/ws/01001000/json/";
+                var resposta = await client.GetStringAsync(uri);
+                Console.WriteLine(resposta);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            */
+            try
+            {
+                string uri = "https://viacep.com.br/ws/01001000/json/";
+                var resposta = await client.GetFromJsonAsync<ViaCep>(uri);
+                Console.WriteLine(resposta.Logradouro);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ DemoAsync2/ViaCep.cs
@@ -0,0 +1,16 @@
+namespace DemoAsync2
+{
+    public class ViaCep
+    {
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string Uf { get; set; }
+        public string Ibge { get; set;}
+        public string Gia { get; set;}
+        public string Ddd { get; set;}
+        public string Siafi { get; set;}
+    }
+}
