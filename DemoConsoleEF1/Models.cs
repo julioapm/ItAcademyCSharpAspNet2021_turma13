f5bc31299570ee2c9c10fd2946736c83ccd92dc0
Júlio Pereira Machado <julioapm@gmail.com>
2021-08-18 17:33:11 -0300

    Código tutorial Microsoft

---
+++ DemoConsoleEF1/Models.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoConsoleEF1
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        public List<Post> Posts { get; } = new List<Post>();
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Blog Blog { get; set; }
+        public int BlogId { get; set; }
+    }
+
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs {get; set;}
+        public DbSet<Post> Posts {get; set;}
+        public string DbPath { get; private set;}
+        public BloggingContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = $"{path}{System.IO.Path.DirectorySeparatorChar}blogging.db";
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={DbPath}");
+        }
+    }
+}
---
+++ DemoConsoleEF1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace DemoConsoleEF1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                /*
+                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                db.Add(new Blog { Url = "http://blogs.msdn.com" });
+                db.SaveChanges();
+                */
+
+                var blogs = db.Blogs.OrderBy(b => b.BlogId).ToList();
+                blogs.ForEach(b => Console.WriteLine($"Id:{b.BlogId} Url:{b.Url}"));
+            }
+        }
+    }
+}
