f4cf91f89dada88018c09e3942d77899aecd26c9
Júlio Pereira Machado <julioapm@gmail.com>
2021-08-20 14:52:13 -0300

    Operações CRUD

--- DemoConsoleEF2/Program.cs
+++ DemoConsoleEF2/Program.cs
@@ -11,11 +11,42 @@ namespace DemoConsoleEF2
         {
             using(var db = new FilmesDbContext())
             {
+                /*
                 Filme f1 = new Filme { Titulo = "Titanic", Ano = 1997 };
                 Filme f2 = new Filme { Titulo = "Titanic 2", Ano = 1998, Duracao = 120 };
                 db.Add(f1);
                 db.Filmes.Add(f2);
                 db.SaveChanges();
+                */
+
+                /*
+                var filmes = db.Filmes.OrderBy(f => f.Ano).ToList();
+                filmes.ForEach(f => Console.WriteLine($"{f.Titulo} - {f.Ano}"));
+                */
+
+                /*
+                var umFilme = db.Filmes.Find(2);
+                if (umFilme != null)
+                {
+                    umFilme.Sinopse = "Novo sinopse";
+                    db.SaveChanges();
+                }
+                */
+
+                /*
+                var umFilme = db.Filmes.Where(f => f.Titulo == "Titanic").FirstOrDefault();
+                if (umFilme != null)
+                {
+                    Console.WriteLine($"{umFilme.Titulo} - {umFilme.Ano}");
+                }
+                */
+
+                var umFilme = db.Filmes.Find(2);
+                if (umFilme != null)
+                {
+                    db.Remove(umFilme);
+                    db.SaveChanges();
+                }
             }
         }
     }
