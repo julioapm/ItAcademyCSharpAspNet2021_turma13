5e89f92ea0120aa2edb6f7b870cebdf386df7577
Júlio Pereira Machado <julioapm@gmail.com>
2021-08-23 16:08:30 -0300

    Buscar por id

--- DemoWsEF1/Controllers/ProdutosController.cs
+++ DemoWsEF1/Controllers/ProdutosController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using DemoWsEF1.Models;
 using DemoWsEF1.Dtos;
-//using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace DemoWsEF1.Controllers
 {
@@ -28,6 +28,32 @@ namespace DemoWsEF1.Controllers
         public IEnumerable<ProdutoDTO> BuscarTodosProdutos()
         {
             return _basedados.Produtos.Select(ProdutoDTO.FromProduto);
+        }
+
+        //GET /produtos/{id}
+        [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public async Task<ActionResult<ProdutoDTO>> BuscarProdutoPorId(int id)
+        {
+            try
+            {
+                var produto = await _basedados.Produtos
+                    .AsNoTracking()
+                    .SingleOrDefaultAsync(p => p.ProdutoId == id);
+                if (produto == null)
+                {
+                    return NotFound();
+                }
+                return ProdutoDTO.FromProduto(produto);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Falha de consultao ao produto de id {id}");
+                throw;
+            }
         }
     }
 }
